343a93fa87c37d001c24872a2fbe726241f1f71e
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-28 21:17:52 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,67 @@ use std::str;
 const HTTP_PORT: u16 = 80;
 const HTTPS_PORT: u16 = 443;
 
+struct Browser {
+    history: VecDeque<String>,
+    current_url: Option<String>,
+    cache: HashMap<String, String>,
+}
+
+impl Browser {
+    fn new() -> Self {
+        Browser {
+            history: VecDeque::new(),
+            current_url: None,
+            cache: HashMap::new(),
+        }
+    }
+
+    fn navigate(&mut self, url: String) {
+        self.history.push_back(url.clone());
+        self.current_url = Some(url);
+    }
+
+    fn back(&mut self) -> Option<String> {
+        self.history.pop_back().map(|url| {
+            self.current_url = self.history.back().cloned();
+            url
+        })
+    }
+
+    fn forward(&mut self) -> Option<String> {
+        self.history.pop_front().map(|url| {
+            self.current_url = self.history.front().cloned();
+            url
+        })
+    }
+
+    fn refresh(&mut self) {
+        if let Some(url) = self.current_url.clone() {
+            self.cache.remove(&url);
+        }
+    }
+
+    fn set_cache(&mut self, url: &str, response: String) {
+        self.cache.insert(url.to_string(), response);
+    }
+
+    fn get_cache(&self, url: &str) -> Option<&String> {
+        self.cache.get(url)
+    }
+}
+
 fn main() {
-    let url = read_user_input("Enter URL: ");
-    let (host, path) = parse_url(&url);
-    let port = get_port(&url);
-    let stream = connect_to_stream(&host, port).await;
-    make_request(&mut stream, &url);
+    let mut browser = Browser::new();
+    loop {
+        let url = read_user_input("Enter URL: ");
+        browser.navigate(url.clone());
+        
+        let (host, path) = parse_url(&url);
+        let port = get_port(&url);
+        let stream = connect_to_stream(&host, port).await;
+        make_request(&mut stream, &url);
+    }
+    Ok(())
 }
 
 async fn make_request(stream: &mut Result<TcpStream, std::io::Error>, host: &str, path: &str) {
@@ -79,6 +134,7 @@ async fn handle_request(stream: &mut TcpStream, host: &str, path: &str) {
     if let Some((headers, body)) = parse_http_response(&response) {
         println!("Headers:\n{}", headers);
         println!("Body:\n{}", body);
+        // render HTML
     } else {
         eprintln!("Failed to parse HTTP response");
     }
