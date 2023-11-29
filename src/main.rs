58316692f01ce9428002d8da356da460753db40e
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-29 15:19:08 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 mod core;
 use core::browser_controls::Browser;
-use core::network_controls::http_response;
 use core::interface_controls::build_interface;
 
 use std::sync::Mutex;
 
 fn main() {
     let browser_mutex = Mutex::new(Browser::new());
-    let network_response = http_response(&browser_mutex)
     if let Err(err) = build_interface(&browser_mutex, &network_response) {
         eprintln!("Browser initialization failed: {:?}", err);
     }
