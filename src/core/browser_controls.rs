1b08165d64b662f49bd84258c0a27341a84d3b00
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-29 11:53:22 -0500

    Update browser_controls.rs

--- src/core/browser_controls.rs
+++ src/core/browser_controls.rs
@@ -5,10 +5,11 @@ struct Browser {
     history: VecDeque<String>,
     current_url: Option<String>,
     cache: HashMap<String, String>,
+    bookmarks: HashMap<String, String>,
 }
 
 impl Browser {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Browser {
             history: VecDeque::new(),
             current_url: None,
@@ -16,36 +17,44 @@ impl Browser {
         }
     }
 
-    fn navigate(&mut self, url: String) {
+    pub fn navigate(&mut self, url: String) {
         self.history.push_back(url.to_string());
         self.current_url = Some(url.to_string());
     }
 
-    fn back(&mut self) -> Option<String> {
+    pub fn back(&mut self) -> Option<String> {
         self.history.pop_back().map(|url| {
             self.current_url = self.history.back().cloned();
             url
         })
     }
 
-    fn forward(&mut self) -> Option<String> {
+    pub fn forward(&mut self) -> Option<String> {
         self.history.pop_front().map(|url| {
             self.current_url = self.history.front().cloned();
             url
         })
     }
 
-    fn refresh(&mut self) {
+    pub fn refresh(&mut self) {
         if let Some(url) = self.current_url.clone() {
             self.cache.remove(&url);
         }
     }
 
-    fn set_cache(&mut self, url: &str, response: String) {
+    pub fn set_cache(&mut self, url: &str, response: String) {
         self.cache.insert(url.to_string(), response);
     }
 
-    fn get_cache(&self, url: &str) -> Option<&String> {
+    pub fn get_cache(&self, url: &str) -> Option<&String> {
         self.cache.get(url)
+    }
+
+    pub fn add_bookmark(&mut self, url: &str, title: &str) {
+        self.bookmarks.insert(title.to_string(), url.to_string());
+    }
+
+    pub fn get_bookmarks(&self) -> &HashMap<String, String> {
+        &self.bookmarks
     }
 }
