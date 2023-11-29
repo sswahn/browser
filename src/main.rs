5aee9658101dc931d86b8b68d68d0b993c96fbc6
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-29 09:06:04 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,62 +9,13 @@ use gtk::prelude::*;
 use gtk::{Label, Button, Entry, Window, WindowType};
 use tokio::task;
 
-//mod core/controls;
+mod core/controls;
 
 const HTTP_PORT: u16 = 80;
 const HTTPS_PORT: u16 = 443;
 
-struct Browser {
-    history: VecDeque<String>,
-    current_url: Option<String>,
-    cache: HashMap<String, String>,
-}
-
-impl Browser {
-    fn new() -> Self {
-        Browser {
-            history: VecDeque::new(),
-            current_url: None,
-            cache: HashMap::new(),
-        }
-    }
-
-    fn navigate(&mut self, url: String) {
-        self.history.push_back(url.to_string());
-        self.current_url = Some(url.to_string());
-    }
-
-    fn back(&mut self) -> Option<String> {
-        self.history.pop_back().map(|url| {
-            self.current_url = self.history.back().cloned();
-            url
-        })
-    }
-
-    fn forward(&mut self) -> Option<String> {
-        self.history.pop_front().map(|url| {
-            self.current_url = self.history.front().cloned();
-            url
-        })
-    }
-
-    fn refresh(&mut self) {
-        if let Some(url) = self.current_url.clone() {
-            self.cache.remove(&url);
-        }
-    }
-
-    fn set_cache(&mut self, url: &str, response: String) {
-        self.cache.insert(url.to_string(), response);
-    }
-
-    fn get_cache(&self, url: &str) -> Option<&String> {
-        self.cache.get(url)
-    }
-}
-
 fn main() {
-    let browser_mutex = Mutex::new(Browser::new());
+    let browser_mutex = Mutex::new(controls::Browser::new());
     build_gui(&browser_mutex)
 }
 
