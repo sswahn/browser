d79bd5cf20fa8dd96a77baba876ad0f287516f76
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-29 12:48:34 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod core;
 use core::browser_controls::Browser;
+use core::interface_controls::build_interface;
 
 use native_tls::{TlsConnector, TlsStream};
 use std::net::TcpStream;
@@ -23,7 +24,7 @@ enum BrowserError {
 
 fn main() {
     let browser_mutex = Mutex::new(Browser::new());
-    if let Err(err) = build_browser(&browser_mutex) {
+    if let Err(err) = build_interface(&browser_mutex) {
         eprintln!("Browser initialization failed: {:?}", err);
     }
 }
