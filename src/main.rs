750f0157925d85c7afcb3012bf7666de6d9101a0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-12-01 16:45:16 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod core;
 use core::browser_controls::Browser;
 use core::interface_controls::build_interface;
+use gust::{Gust, Button, TextBox, Label, Styles};
 
 fn main() {
     let browser = Browser::new();
